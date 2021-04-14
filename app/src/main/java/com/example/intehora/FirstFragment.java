0034497b59645322d7bc271ba2672ba8bc8d0015
Hedquist <vilhelm.hedquist@hotmail.com>
2021-04-14 13:53:41 +0200

    removed second button function

--- app/src/main/java/com/example/intehora/FirstFragment.java
+++ app/src/main/java/com/example/intehora/FirstFragment.java
@@ -30,12 +30,5 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
-        view.findViewById(R.id.second_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_blankFragment);
-            }
-        });
     }
 }
