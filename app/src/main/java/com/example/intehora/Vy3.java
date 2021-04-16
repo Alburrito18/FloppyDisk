ef2d7c9848017f013bbfaf660fe159080681eb50
Alburrito18 <albertlund18@gmail.com>
2021-04-16 12:06:31 +0200

    Började med vy 3

--- app/src/main/java/com/example/intehora/Vy3.java
+++ app/src/main/java/com/example/intehora/Vy3.java
@@ -2,9 +2,7 @@ package com.example.intehora;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PersonalInfo#newInstance} factory method to
+ * Use the {@link Vy3#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PersonalInfo extends Fragment {
+public class Vy3 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +24,7 @@ public class PersonalInfo extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public PersonalInfo() {
+    public Vy3() {
         // Required empty public constructor
     }
 
@@ -36,11 +34,11 @@ public class PersonalInfo extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PersonalInfo.
+     * @return A new instance of fragment Vy3.
      */
     // TODO: Rename and change types and number of parameters
-    public static PersonalInfo newInstance(String param1, String param2) {
-        PersonalInfo fragment = new PersonalInfo();
+    public static Vy3 newInstance(String param1, String param2) {
+        Vy3 fragment = new Vy3();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,21 +59,6 @@ public class PersonalInfo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-
-        return inflater.inflate(R.layout.fragment_personal_info, container, false);
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(PersonalInfo.this)
-                        .navigate(R.id.action_personalInfo_to_vy3);
-            }
-        });
-
+        return inflater.inflate(R.layout.fragment_vy3, container, false);
     }
 }
