abbea3b8e54d8d47bfe98032d9773f732936477d
Hedquist <vilhelm.hedquist@hotmail.com>
2021-04-22 16:20:48 +0200

    made generator class singleton
    Co-Authored-By: Alburrito18 <57448414+Alburrito18@users.noreply.github.com>

--- app/src/main/java/com/example/NoSound/IdNumberGenerator.java
+++ app/src/main/java/com/example/NoSound/IdNumberGenerator.java
@@ -3,7 +3,26 @@ package com.example.NoSound;
 public class IdNumberGenerator {
     private String Identifier;
 
+    private static IdNumberGenerator instance;
+
+    private IdNumberGenerator(){}
+
+    public static IdNumberGenerator getInstance() {
+        if(instance == null) {
+            instance = new IdNumberGenerator();
+        }
+        return instance;
+    }
+
     private String getIteration() {
+        /* if (file found) {
+                return(get it)
+           }
+           else {
+                saveIteration(0)
+                return 0
+           }
+         */
         return null;
     }
 
