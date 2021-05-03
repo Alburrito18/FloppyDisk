3de182c5ec76eed63926114c3b173e3f54b51db0
Hedquist <vilhelm.hedquist@hotmail.com>
2021-05-03 14:54:13 +0200

    Implemented generateFilterCode
    I think it should fully work

--- app/src/main/java/com/example/NoSound/ThirdFragment.java
+++ app/src/main/java/com/example/NoSound/ThirdFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -59,6 +60,107 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Log.d("codeTag", generateFilterCode("HN1 - Svart", true, true, false, true, "Blå", "Blå", true));
         return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+
+    private String generateFilterCode(String filter, boolean cord, boolean rightConch,
+                                      boolean leftConch, boolean detect, String rightColour,
+                                      String leftColour, boolean triple) {
+        String output = "";
+
+        //Add filter to output
+        if(filter.equals("HN0 - Magenta") || filter.equals("HN1 - Svart") ||
+           filter.equals("HN2 - Grön") || filter.equals("HN3 - Guld") ||
+           filter.equals("HN4 - Grå") || filter.equals("HN5 - Orange") ||
+           filter.equals("HN6 - Gul") || filter.equals("HN7 - Vit")) {
+            output = output + filter.substring(0, 3);
+        } else {
+            return "Illegal filter choice";
+        }
+
+        //Add cord to output
+        if(!triple) {
+            if(cord && !(rightConch && leftConch)) {
+                output = output + "M";
+            } else if(!(rightConch && leftConch)) {
+                output = output + "Z";
+            }
+        } else {
+            if(cord) {
+                output = output + "M";
+            } else {
+                output = output + "Z";
+            }
+        }
+
+        //Add conch to output
+        if(!triple) {
+            if(rightConch && leftConch) {
+                output = output + "SNT";
+            } else if(rightConch && !leftConch) {
+                output = output + "RNT";
+            } else if(leftConch) {
+                output = output + "LNT";
+            }
+        }
+
+        //Add detect to output
+        if(detect) {
+            output = output + "D";
+        }
+
+        //Add colour to output
+        if(!(leftColour.equals("Transparent") && rightColour.equals("Transparent"))) {
+            output = output + colourNameToCode(rightColour);
+            if(!leftColour.equals(rightColour)) {
+                output = output + "+" + colourNameToCode(leftColour);
+            }
+        }
+
+        if(triple) {
+            output = output + "+" + output.substring(0, 3);
+
+            if(leftConch && !rightConch) {
+                output = output + "LNT";
+            } else if (rightConch && !leftConch) {
+                output = output + "RNT";
+            } else {
+                return "Illegal combination of conch/triple";
+            }
+
+            if(detect) {
+                output = output + "D";
+            }
+
+            if(leftConch && !leftColour.equals("Transparent")) {
+                output = output + colourNameToCode(leftColour);
+            } else if(rightConch && !rightColour.equals("Transparent")) {
+                output = output + colourNameToCode(rightColour);
+            }
+        }
+
+        return output;
+    }
+
+    private String colourNameToCode(String colour) {
+        switch (colour) {
+            case "Transparent" :
+                return "0";
+            case "Blå" :
+                return "1";
+            case "Röd" :
+                return "2";
+            case "Gul" :
+                return "3";
+            case "Grön" :
+                return "4";
+            case "Orange" :
+                return "5";
+            case "Lila" :
+                return "6";
+            default :
+                return "Illegal colour choice";
+        }
     }
 }
